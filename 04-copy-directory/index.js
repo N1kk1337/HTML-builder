cd140a01675f24ccd725df6f76283c391ed82945
Nikki NoLife <nikkinolife@gmail.com>
2022-11-07 23:07:18 +0400

    feature: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,39 @@
+const fs = require("fs");
+const path = require("path");
 
+const PATH = path.join(__dirname, "files");
+
+async function copyDirectory() {
+  const projectFolder = PATH + "-copy";
+  await fs.promises.rm(projectFolder, { force: true, recursive: true });
+  const dirCreation = await fs.promises.mkdir(projectFolder, {
+    recursive: true,
+  });
+  makeCopy(projectFolder);
+
+  return dirCreation;
+}
+
+async function makeCopy(baseFolder) {
+  fs.promises
+    .readdir(PATH, { withFileTypes: true })
+    .then(async (files) => {
+      try {
+        for (let file of files) {
+          if (file.isFile()) {
+            await fs.promises.copyFile(
+              path.join(PATH, file.name),
+              path.join(baseFolder, file.name)
+            );
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+copyDirectory().catch(console.error);
